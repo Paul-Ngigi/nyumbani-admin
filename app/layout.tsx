336968d00e8e74446c342521dcc9282b56b405d2
Paul-Ngigi <paulkush7777@gmail.com>
2024-06-14 07:49:49 +0300

    update theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <AppContext>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
